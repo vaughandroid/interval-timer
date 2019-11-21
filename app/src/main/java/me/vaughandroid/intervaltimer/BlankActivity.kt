db920284d54989eddb09f070503645a588d5d23d
Chris Vaughan <Chris.Vaughan01@bbc.co.uk>
2019-11-21 18:16:14 +0000

    Simplify BlankActivity.

--- app/src/main/java/me/vaughandroid/intervaltimer/BlankActivity.kt
+++ app/src/main/java/me/vaughandroid/intervaltimer/BlankActivity.kt
@@ -1,11 +1,5 @@
 package me.vaughandroid.intervaltimer
 
 import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
 
-class BlankActivity : AppCompatActivity() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
-}
+class BlankActivity : AppCompatActivity()
