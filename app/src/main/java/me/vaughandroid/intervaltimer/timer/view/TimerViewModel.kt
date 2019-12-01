f842ccebd77bd3b1c09f9741dc57446ac3d83177
Chris Vaughan <Chris.Vaughan01@bbc.co.uk>
2019-12-01 09:22:30 +0000

    TimerViewModel converts data from the model into view data.

--- app/src/main/java/me/vaughandroid/intervaltimer/timer/view/TimerViewModel.kt
+++ app/src/main/java/me/vaughandroid/intervaltimer/timer/view/TimerViewModel.kt
@@ -3,8 +3,12 @@ package me.vaughandroid.intervaltimer.timer.view
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import me.vaughandroid.intervaltimer.time.DurationFormatter
+import me.vaughandroid.intervaltimer.timer.domain.TimerModel
 
-class TimerViewModel : ViewModel() {
+class TimerViewModel(
+    timerModel: TimerModel
+) : ViewModel() {
 
     private val mutableLiveData =
         MutableLiveData<TimerViewData>()
@@ -12,7 +16,11 @@ class TimerViewModel : ViewModel() {
     val viewDataLiveData: LiveData<TimerViewData> = mutableLiveData
 
     init {
-        mutableLiveData.value = TimerViewData("", "", "")
+        mutableLiveData.value = TimerViewData(
+            setsText = timerModel.currentSets.toString(),
+            workTimeText = DurationFormatter.toDisplayString(timerModel.currentWorkTime),
+            restTimeText= DurationFormatter.toDisplayString(timerModel.currentRestTime)
+        )
     }
 
 }
--- app/src/test/java/me/vaughandroid/intervaltimer/timer/view/TimerViewModelTests.kt
+++ app/src/test/java/me/vaughandroid/intervaltimer/timer/view/TimerViewModelTests.kt
@@ -2,7 +2,12 @@ package me.vaughandroid.intervaltimer.timer.view
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.lifecycle.Observer
-import com.google.common.truth.Truth
+import com.google.common.truth.Truth.assertThat
+import me.vaughandroid.intervaltimer.configuration.data.FakeConfigurationStore
+import me.vaughandroid.intervaltimer.configuration.domain.Configuration
+import me.vaughandroid.intervaltimer.time.minutes
+import me.vaughandroid.intervaltimer.time.seconds
+import me.vaughandroid.intervaltimer.timer.domain.TimerModel
 import org.junit.Rule
 import org.junit.Test
 
@@ -21,11 +26,35 @@ class TimerViewModelTests {
         viewModel.viewDataLiveData.observeForever(spyViewDataObserver)
 
         // Then
-        Truth.assertThat(spyViewDataObserver.receivedViewData).hasSize(1)
+        assertThat(spyViewDataObserver.receivedViewData).hasSize(1)
     }
 
-    private fun createViewModel(): TimerViewModel {
-        return TimerViewModel()
+    @Test
+    fun `model is correctly transformed into view data`() {
+        // Given
+        val configuration = Configuration(
+            sets = 7,
+            workTime = 11.seconds,
+            restTime = 1.minutes + 13.seconds
+        )
+        val viewModel = createViewModel(configuration)
+        val spyViewDataObserver = SpyViewDataObserver()
+
+        // When
+        viewModel.viewDataLiveData.observeForever(spyViewDataObserver)
+
+        // Then
+        val expectedViewData = TimerViewData(
+            setsText = "7",
+            workTimeText = "0:11",
+            restTimeText = "1:13"
+        )
+        assertThat(spyViewDataObserver.receivedViewData.first()).isEqualTo(expectedViewData)
+    }
+
+    private fun createViewModel(configuration: Configuration = Configuration()): TimerViewModel {
+        val timerModel = TimerModel(FakeConfigurationStore(configuration))
+        return TimerViewModel(timerModel)
     }
 
 }
