c979f8591f766be4a2878a8276b3d7c034a4705f
Chris Vaughan <Chris.Vaughan01@bbc.co.uk>
2019-12-03 21:23:42 +0000

    Begin expanding tests for TimerModel.

--- app/src/test/java/me/vaughandroid/intervaltimer/timer/domain/TimerModelTests.kt
+++ app/src/test/java/me/vaughandroid/intervaltimer/timer/domain/TimerModelTests.kt
@@ -4,6 +4,8 @@ import com.google.common.truth.Truth.assertThat
 import me.vaughandroid.intervaltimer.configuration.data.FakeConfigurationStore
 import me.vaughandroid.intervaltimer.configuration.domain.Configuration
 import me.vaughandroid.intervaltimer.time.SystemTimeProvider
+import me.vaughandroid.intervaltimer.time.TestTimeProvider
+import me.vaughandroid.intervaltimer.time.minutes
 import me.vaughandroid.intervaltimer.time.seconds
 import org.junit.Test
 
@@ -26,6 +28,43 @@ class TimerModelTests {
         assertThat(model.currentSets).isEqualTo(12)
         assertThat(model.currentWorkTime).isEqualTo(37.seconds)
         assertThat(model.currentRestTime).isEqualTo(43.seconds)
+    }
+
+    @Test
+    fun `when not started, timers are not updated as time passes`() {
+        // Given
+        val testTimeProvider = TestTimeProvider()
+        val storedConfiguration = Configuration(
+            workTime = 2.minutes,
+            restTime = 2.minutes
+        )
+        val model = TimerModel(FakeConfigurationStore(storedConfiguration), testTimeProvider)
+
+        // When
+        testTimeProvider.advanceTime(1.minutes)
+
+        // Then
+        assertThat(model.currentWorkTime).isEqualTo(2.minutes)
+        assertThat(model.currentRestTime).isEqualTo(2.minutes)
+    }
+
+    @Test
+    fun `when started, work time is updated as time passes`() {
+        // Given
+        val testTimeProvider = TestTimeProvider()
+        val storedConfiguration = Configuration(
+            workTime = 2.minutes,
+            restTime = 2.minutes
+        )
+        val model = TimerModel(FakeConfigurationStore(storedConfiguration), testTimeProvider)
+        model.start()
+
+        // When
+        testTimeProvider.advanceTime(1.minutes)
+
+        // Then
+        assertThat(model.currentWorkTime).isEqualTo(1.minutes)
+        assertThat(model.currentRestTime).isEqualTo(2.minutes)
     }
 
 }
